ba5e0d1366469f77f22fa0c8429e9f96fbee0654
Karishma Vakil <karishmavakil@Karishmas-MacBook-Pro.local>
2018-08-06 13:15:30 +0530

    set up

---
+++ src/main/java/item.java
@@ -0,0 +1,7 @@
+class item {
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("Item");
+    }
+
+}
---
+++ test/main/java/itemTest.java
@@ -0,0 +1,7 @@
+package main.java;
+
+import junit.framework.TestCase;
+
+public class itemTest extends TestCase {
+
+}
